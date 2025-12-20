f23c6fbc0ff85270d53e5d3160fa756a7a83506d
Ambareesh A <57589799+4mbareesh@users.noreply.github.com>
2025-12-20 18:10:28 +0530

    fix: update logging for Google Sheets credentials in getSheetsClient function

--- netlify/functions/offers2.js
+++ netlify/functions/offers2.js
@@ -3,12 +3,13 @@ const { google } = require("googleapis");
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 
+console.log("RAW CREDS ENV (first 200 chars):",
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+);
+
 async function getSheetsClient() {
   const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
   credentials.private_key = credentials.private_key.replace(/\\n/g, '\n')
-  console.log("RAW CREDS ENV (first 200 chars):",
-    process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON?.slice(0, 200)
-  );
 
   const auth = new google.auth.GoogleAuth({
     credentials,
